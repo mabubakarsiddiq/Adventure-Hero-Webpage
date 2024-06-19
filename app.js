68daa322fe143ad5a0c57814b514abac660a5275
mabubakarsiddiq <mabubakarsiddiq11@gmail.com>
2024-06-18 22:13:25 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+
+
+const menuBtn = document.querySelector(".menu-btn")
+const navLinks = document.querySelector(".nav_links")
+
+menuBtn.addEventListener("click",() => {
+navLinks.classList.toggle("mobile-menu")
+})
+
+
